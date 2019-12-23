9820554fe7de5f01aa156fa17eed9beeaf4cca05
Aamir Farooq <aamir@farooq.xyz>
2019-12-23 09:15:03 +0500

    Able to delete phrases

--- src/components/Phrase.vue
+++ src/components/Phrase.vue
@@ -1,26 +1,40 @@
 <template>
-  <div class="columns is-mobile">
-    <div class="column" v-if="!editMode">
-      <a class="button is-info is-large">{{ phrase }}</a>
-    </div>
-    <div class="column is-four-fifths" v-if="editMode">
-      <a class="button is-info is-large">{{ phrase }}</a>
-    </div>
-    <div class="column is-one-fifth" v-if="editMode">
-      <a class="button is-danger is-large">del</a>
-    </div>
-  </div>
+	<div class="columns is-mobile" :class="deleted ? 'grayscale' : ''">
+		<div class="column" v-if="!editMode">
+			<a class="button is-info is-large">{{ phrase }}</a>
+		</div>
+		<div class="column is-four-fifths" v-if="editMode">
+			<a class="button is-info is-large">{{ phrase }}</a>
+		</div>
+		<div class="column is-one-fifth" v-if="editMode">
+			<a @click="delPhrase" class="button is-danger is-large">del</a>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "Phrase",
-  props: ["phrase", "editMode"]
+	name: "Phrase",
+	props: ["phrase", "editMode"],
+	data() {
+		return {
+			deleted: false
+		};
+	},
+	methods: {
+		delPhrase() {
+			this.deleted = !this.deleted;
+			this.$emit("delete", this.phrase.id);
+		}
+	}
 };
 </script>
 
 <style>
-.button {
-  margin-bottom: 10px;
+.columns {
+	margin-bottom: 10px;
+}
+.grayscale {
+	background-color: rgb(151, 151, 151);
 }
 </style>
--- src/components/Phrases.vue
+++ src/components/Phrases.vue
@@ -1,40 +1,86 @@
 <template>
-  <div>
-    <div class="phrase-container">
-      <Phrase
-        v-for="phrase in phrasesData"
-        :key="phrase.id"
-        :phrase="phrase.phraseText"
-        :editMode="editMode"
-      />
-      <a v-if="!editMode" @click="editMode = !editMode" class="button is-danger">Edit</a>
-      <a v-if="editMode" @click="editMode = !editMode" class="button is-primary">Save</a>
-    </div>
-  </div>
+	<div>
+		<div class="phrase-container">
+			<Phrase
+				v-for="phrase in phrases"
+				:key="phrase.id"
+				:phrase="phrase.phraseText"
+				:editMode="editMode"
+				v-on:delete="toDelete(phrase.id)"
+			/>
+			<a
+				v-if="!editMode"
+				@click="editMode = !editMode"
+				class="button is-danger"
+				>Edit</a
+			>
+			<div class="columns is-mobile" v-if="editMode">
+				<div class="column is-half">
+					<a @click="editMode = !editMode" class="button is-danger"
+						>Cancel</a
+					>
+				</div>
+				<div class="column is-half">
+					<a @click="save" class="button is-primary">Save</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import uuid from "uuid";
 import Phrase from "@/components/Phrase";
 export default {
-  name: "Phrases",
-  components: {
-    Phrase
-  },
-  data() {
-    return {
-      phrasesData: [
-        { id: uuid.v4(), phraseText: "Thank you!" },
-        { id: uuid.v4(), phraseText: "Uh oh stinky" }
-      ],
-      editMode: false
-    };
-  }
+	name: "Phrases",
+	components: {
+		Phrase
+	},
+	data() {
+		return {
+			phrases: [
+				{ id: uuid.v4(), phraseText: "Thank you!" },
+				{ id: uuid.v4(), phraseText: "Uh oh stinky" }
+			],
+			delete: [],
+			editMode: false
+		};
+	},
+	methods: {
+		toDelete(id) {
+			// console.log(this.delete);
+			// console.log(id);
+			var found = false;
+			for (var i = 0; i < this.delete.length; i++) {
+				// console.log(this.delete[i]);
+				if (this.delete[i] == id) {
+					console.log("FOUND");
+
+					found = true;
+				}
+			}
+			if (found) {
+				console.log("Cancelled delete");
+				this.delete.splice(i, 1);
+			} else {
+				this.delete.push(id);
+				console.log("Deleting", id);
+			}
+		},
+		save() {
+			this.delete.map(deleteItem => {
+				this.phrases = this.phrases.filter(
+					phrase => phrase.id != deleteItem
+				);
+			});
+			this.editMode = !this.editMode;
+		}
+	}
 };
 </script>
 
 <style>
 .phrase-container {
-  padding: 0 20px;
+	padding: 0 20px;
 }
 </style>
