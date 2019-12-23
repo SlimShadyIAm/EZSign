3fae3062279a74bc2039245f5c4d83d495656baf
Aamir Farooq <aamir@farooq.xyz>
2019-12-23 10:44:41 +0500

    Various icon, button style changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,30 +1,39 @@
 <template>
-    <footer v-if="$route.name != 'home'" class="footer">
-        <div class="content has-text-centered">
-            <div class="columns is-mobile">
-                <div class="column is-half">
-                    <router-link
-                        class="button is-info"
-                        v-if="$routerHistory.hasPrevious()"
-                        :to="{ path: $routerHistory.previous().path }"
-                    >Back</router-link>
-                </div>
-                <div class="column is-half">
-                    <router-link to="/" class="button is-info">Home</router-link>
-                </div>
-            </div>
-        </div>
-    </footer>
+	<footer v-if="$route.name != 'home'" class="footer">
+		<div class="content has-text-centered">
+			<div class="columns is-mobile">
+				<div class="column is-half is-offset-one-quarter">
+					<router-link
+						class="button is-danger"
+						v-if="$routerHistory.hasPrevious()"
+						:to="{ path: $routerHistory.previous().path }"
+						><UndoIcon></UndoIcon>Back</router-link
+					>
+				</div>
+			</div>
+		</div>
+	</footer>
 </template>
 
 <script>
+import UndoIcon from "vue-material-design-icons/Undo.vue";
+
+import "vue-material-design-icons/styles.css";
 export default {
-    name: "Footer"
+	name: "Footer",
+	components: {
+		UndoIcon
+	}
 };
 </script>
 
 <style>
 .button {
-    width: 100%;
+	width: 100%;
+}
+span {
+	margin-right: 8px;
+	margin-bottom: 8px;
+	font-size: 20px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,43 +1,52 @@
 <template>
-    <section class="hero is-medium is-info is-bold">
-        <div class="hero-head">
-            <nav class="navbar">
-                <div class="container">
-                    <div class="navbar-brand">
-                        <router-link to="/" class="navbar-item">EZ-Sign</router-link>
-                    </div>
-                </div>
-            </nav>
-        </div>
-        <div class="hero-body">
-            <div class="container">
-                <h1 class="title">
-                    <span class="route">{{ text }}</span>
-                </h1>
-                <h2 class="subtitle">{{ subtext }}</h2>
-            </div>
-        </div>
-    </section>
+	<section class="hero is-info is-bold">
+		<div class="hero-head">
+			<nav class="navbar">
+				<div class="container">
+					<div class="navbar-brand">
+						<router-link to="/" class="navbar-item"
+							><HomeIcon />Home</router-link
+						>
+					</div>
+				</div>
+			</nav>
+		</div>
+		<div class="hero-body">
+			<div class="container">
+				<h1 class="title">
+					<span class="route">{{ text }}</span>
+				</h1>
+				<h2 class="subtitle">{{ subtext }}</h2>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
-export default { name: "Header", props: ["text", "subtext"] };
+import HomeIcon from "vue-material-design-icons/Home.vue";
+
+export default {
+	name: "Header",
+	components: { HomeIcon },
+	props: ["text", "subtext"]
+};
 </script>
 
 <style>
 .hero {
-    margin-bottom: 20px;
+	margin-bottom: 20px;
 }
 .route {
-    font-style: italic;
-    font-size: 26px;
+	font-style: italic;
+	font-size: 26px;
 }
 
 .subtitle {
-    font-size: 22px !important;
+	font-size: 22px !important;
+	font-weight: 400 !important;
 }
 
 .navbar-brand {
-    font-weight: 800;
+	font-weight: 800;
 }
 </style>
