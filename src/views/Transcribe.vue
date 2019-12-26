45aff5ffbcf766dbac9bde3b1b4b4712ef61bf71
Aamir Farooq <aamir@farooq.xyz>
2019-12-26 22:22:12 +0500

    Display sample text when file uploaded to 'Record Sign language' input

--- src/views/Transcribe.vue
+++ src/views/Transcribe.vue
@@ -8,21 +8,38 @@
                 </div>
                 <div class="message-body">
                     Hello! My name is John Doe. This app will translate my sign
-                    language. Press "Start Transcribing" and record me!
+                    language. Press
+                    <b>"Record Sign Langauge"</b> and record me!
                 </div>
             </article>
-            <a href class="button">Start Transcribing</a>
+            <!-- <input type="file" accept="video" class="button" value="Start Transcribing" /> -->
+            <label for="file-upload" class="custom-file-upload button">Record Sign Language</label>
+            <input id="file-upload" type="file" accept="video" @change="modalActive = !modalActive" />
         </div>
+        <Transcribed
+            text="Iced coffee"
+            :modalActive="modalActive"
+            v-on:close="modalActive = !modalActive"
+        ></Transcribed>
     </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
+import Transcribed from "@/components/Transcribed";
+// import VideoIcon from "vue-material-design-icons/Video.vue";
 
 export default {
     name: "Transcribe",
     components: {
-        Header
+        Header,
+        Transcribed
+        // VideoIcon
+    },
+    data() {
+        return {
+            modalActive: false
+        };
     }
 };
 </script>
@@ -37,4 +54,13 @@ export default {
 .flex-container > *:last-child {
     align-self: flex-end;
 }
+input[type="file"] {
+    display: none;
+}
+/* .custom-file-upload {
+    border: 1px solid #ccc;
+    display: inline-block;
+    padding: 6px 12px;
+    cursor: pointer;
+} */
 </style>
