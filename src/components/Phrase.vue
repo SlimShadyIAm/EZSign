7f75c7ca3cf398136970ec929d719c1ccd94e650
Aamir Farooq <aamir@farooq.xyz>
2019-12-26 22:02:05 +0500

    Add modal for displaying text when transcribed text

--- src/components/Phrase.vue
+++ src/components/Phrase.vue
@@ -1,53 +1,60 @@
 <template>
-	<div class="columns is-mobile" :class="deleted ? 'grayscale' : ''">
-		<div class="column" v-if="!editMode">
-			<a class="button is-info is-large">{{ phrase }}</a>
-		</div>
-		<div class="column is-four-fifths" v-if="editMode">
-			<a class="button is-info is-large">{{ phrase }}</a>
-		</div>
-		<div class="column is-one-fifth" v-if="editMode">
-			<a @click="delPhrase" class="button is-danger is-large"
-				><TrashIcon
-			/></a>
-		</div>
-	</div>
+    <div class="columns is-mobile" :class="deleted ? 'grayscale' : ''">
+        <div class="column" v-if="!editMode">
+            <a class="button is-info is-large" @click="modalActive = !modalActive">{{ phrase }}</a>
+        </div>
+        <div class="column is-four-fifths" v-if="editMode">
+            <a class="button is-info is-large">{{ phrase }}</a>
+        </div>
+        <div class="column is-one-fifth" v-if="editMode">
+            <a @click="delPhrase" class="button is-danger is-large">
+                <TrashIcon />
+            </a>
+        </div>
+        <Transcribed
+            :text="phrase"
+            :modalActive="modalActive"
+            v-on:close="modalActive = !modalActive"
+        ></Transcribed>
+    </div>
 </template>
 
 <script>
 import TrashIcon from "vue-material-design-icons/Delete.vue";
-
+import Transcribed from "@/components/Transcribed";
 export default {
-	name: "Phrase",
-	props: ["phrase", "editMode"],
-	components: {
-		TrashIcon
-	},
-	data() {
-		return {
-			deleted: false
-		};
-	},
-	methods: {
-		delPhrase() {
-			this.deleted = !this.deleted;
-			this.$emit("delete", this.phrase.id);
-		}
-	}
+    name: "Phrase",
+    props: ["phrase", "editMode"],
+    components: {
+        TrashIcon,
+        Transcribed
+    },
+    data() {
+        return {
+            deleted: false,
+            modalActive: false
+        };
+    },
+    methods: {
+        delPhrase() {
+            this.deleted = !this.deleted;
+            this.$emit("delete", this.phrase.id);
+        }
+    }
 };
 </script>
 
 <style>
 .columns {
-	margin-bottom: 10px;
+    margin-bottom: 10px;
 }
 .grayscale {
-	background-color: rgb(151, 151, 151);
+    background-color: rgb(151, 151, 151);
 }
 
 span {
-	display: block;
-	margin-left: 0;
-	font-size: 24px;
+    display: block;
+    margin-left: 0;
+    font-size: 24px;
 }
 </style>
---
+++ src/components/Transcribed.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="modal" :class="modalActive ? 'is-active' : ''">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">Transcribed text</p>
+                <button @click="closeModal" class="delete" aria-label="close"></button>
+            </header>
+            <section class="modal-card-body">{{text}}</section>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Transcribed",
+    props: ["text", "modalActive"],
+    methods: {
+        closeModal() {
+            this.modalActive = !this.modalActive;
+            this.$emit("close", this.modalActive);
+        }
+    }
+};
+</script>
+
+<style>
+.modal-card {
+    height: 100%;
+}
+.modal-card-head,
+.modal-card-body {
+    border-bottom: none;
+    border-bottom-color: #fff;
+}
+
+.delete {
+    min-width: 40px !important;
+    max-width: 40px !important;
+    min-height: 40px !important;
+    max-height: 40px !important;
+    margin-bottom: 20px;
+}
+
+.modal-card-head {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column-reverse;
+}
+
+.modal-card-body {
+    font-size: 48px;
+    font-weight: 600;
+}
+</style>
