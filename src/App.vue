577c369e5910436b57995f5480fe091a192af867
Aamir Farooq <aamir@farooq.xyz>
2019-12-30 21:58:59 +0500

    test a fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <router-view />
-        <Footer></Footer>
+        <Footer v-if="$route.name != 'home'"></Footer>
     </div>
 </template>
 
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,19 @@
 <template>
-	<footer v-if="$route.name != 'home'" class="footer">
-		<div class="content has-text-centered">
-			<div class="columns is-mobile">
-				<div class="column is-half is-offset-one-quarter">
-					<router-link
-						class="button is-danger"
-						v-if="$routerHistory.hasPrevious()"
-						:to="{ path: $routerHistory.previous().path }"
-						><UndoIcon></UndoIcon>Back</router-link
-					>
-				</div>
-			</div>
-		</div>
-	</footer>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <div class="columns is-mobile">
+                <div class="column is-half is-offset-one-quarter">
+                    <router-link
+                        class="button is-danger"
+                        v-if="$routerHistory.hasPrevious()"
+                        :to="{ path: $routerHistory.previous().path }"
+                    >
+                        <UndoIcon></UndoIcon>Back
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -20,20 +21,20 @@ import UndoIcon from "vue-material-design-icons/Undo.vue";
 
 import "vue-material-design-icons/styles.css";
 export default {
-	name: "Footer",
-	components: {
-		UndoIcon
-	}
+    name: "Footer",
+    components: {
+        UndoIcon
+    }
 };
 </script>
 
 <style>
 .button {
-	width: 100%;
+    width: 100%;
 }
 span {
-	margin-right: 8px;
-	margin-bottom: 8px;
-	font-size: 20px;
+    margin-right: 8px;
+    margin-bottom: 8px;
+    font-size: 20px;
 }
 </style>
